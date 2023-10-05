2252a3d0c27bcd6db2dabaf1e95844fbbacbe78a
Stanley Wong <stanley0620@gmail.com>
2023-10-04 23:52:11 -0400

    added proposal

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ df = pd.DataFrame(
        {"command": "st.time_input", "rating": 3, "is_widget": True},
    ]
 )
+st.title('ML Project Group 99 - StockWise')
+st.subheader("Introduction")
+st.text('Stock prices are known for their high levels of volatility, which can be attributed to a myriad of factors, including economic conditions, geopolitical events, investor sentiment, and company-specific news. This inherent volatility makes predicting stock prices a challenging task. Machine learning (ML) techniques have gained popularity in the realm of stock price prediction due to their ability to handle complex data patterns and adapt to changing market dynamics and uncover hidden patterns and relationships that human analysts may overlook. The benefits of using ML for stock price prediction include improved accuracy compared to traditional methods, the ability to process vast amounts of data in real time, and the potential for automation, enabling investors to make more informed decisions and better manage risk in the highly dynamic world of finance.')
+st.subheader("Literature Review")
+st.text("Most algorithms used in stock price prediction are some forms of time series analysis models, recurrent neural networks, or deep learning models. Out of many time series models, Autoregressive Integrated Moving Average is very popular for modeling and forecasting time series data, including stock prices. They can capture trends, seasonality, and autocorrelation in the data and are stronger at producing short-term predictions [2]. The Long-Short Term Memory network is an advanced recurrent neural network that is stronger for long-term stock prediction due to its ability to remember sequential information and have “contextual memory” for past information [1].")
+st.subheader("Problem Statement")
+st.text("We aim to predict the future price movements of stocks relative to the future price movement of a synthetic index composed of NASDAQ-listed stocks. Specifically, we are looking to accurately predict the weighted average price 1 minute into the future.")
+st.subheader("Dataset")
+st.text("We chose the Kaggle dataset from the competition “Optiver - Trading at the Close: Predict US stocks closing movements” which contains historical data for the daily ten-minute closing auction on the NASDAQ stock exchange. The training dataset has 5,237,981 rows of various stocks and their features and the target price after 1 minute. The test dataset has 33000 test cases. There are 15 features in the dataset, some of which include the stock_id, date_id, matched_size which is the amount that can be matched at the current reference price (in USD), weight average price, and other features.")
+st.subheader("Methods")
+st.text("First, we will develop a baseline model by running a deep neural network on all features without any feature extraction or dimension reduction.")
+st.text("We will then try to extract features using PCA and then train an LSTM and then an ARIMA on these extracted features.")
+st.subheader("Results & Discussion")
+st.text("To quantitatively assess the performance of our machine learning models, we have selected a set of metrics that collectively provide a robust framework for assessing the effectiveness and reliability of our research outcomes. We will use mean absolute error (MAE) to provide a sense of the magnitude of errors in predictions. Mean squared error (MSE) will be employed since it penalizes larger errors more heavily than MAE. We will also use r-squared (R2) since it indicates goodness of fit.")
 
 # Display text
 st.text('Fixed width text')
