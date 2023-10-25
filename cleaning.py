171c12855cb99e7cec7bf664922f2d08b7b0e1f1
djliu9049890 <55365026+djliu9049890@users.noreply.github.com>
2023-10-25 19:40:21 -0400

    conda env yml

--- cleaning.py
+++ cleaning.py
@@ -1,4 +1,9 @@
 import pandas as pd
+from AutoClean import AutoClean
 
 df = pd.read_csv("optiver-trading-at-the-close/train.csv")
 print(df.head())
+print(df.isna().any().any())
+#pipeline = AutoClean(df)
+print(df.isna().any().any())
+print("done")
