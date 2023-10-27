d19413beaf5345e1280206455cb931de635975ae
djliu9049890 <55365026+djliu9049890@users.noreply.github.com>
2023-10-27 17:22:10 -0400

    done cleaning

--- cleaning.py
+++ cleaning.py
@@ -16,8 +16,21 @@ print(df['date_id'].value_counts())
 print(len(df))
 df = df.dropna()
 print(len(df))
-df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
-print(len(df))
+#df = df[(np.abs(stats.zscore(df)) < 3).all(axis=1)]
+for col in df.columns:
+    if "id" not in col:
+        df = df[np.abs(df[col]-df[col].mean()) <= (3*df[col].std())]
+#     q_low = df[col].quantile(0.01)
+#     q_hi  = df[col].quantile(0.99)
+#     print("xxxxx")
+
+#     print(len(df[col] < q_hi))
+#     print(len(df[col] > q_low))
+#     df = df[(df[col] < q_hi) & (df[col] > q_low)]
+    print("length check:", len(df))
+    print("col check:", df[col])
+print("length:", len(df))
 #pipeline = AutoClean(df, mode = 'manual', outliers = 'auto', missing_num='auto')
 print(df.isna().any().any())
+#df.to_csv('train_clean.csv', index=False)
 print("done")
