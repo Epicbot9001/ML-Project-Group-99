fe4f6b140aea864515a2940b884bfdd7e5211fd9
shreyasgoyal213 <the.shreyasgoyal@gmail.com>
2023-12-01 14:51:39 -0500

    Model v2: Individual stocks.

--- lstm.py
+++ lstm.py
@@ -4,6 +4,73 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import backend as K
 import numpy as np
+from tensorflow.keras.optimizers import Adam
+
+def custom_metric(y_true, y_pred):
+    threshold = 0.5
+    absolute_difference = K.abs((y_true - y_pred) / y_true)
+    correct_predictions = K.cast(absolute_difference <= threshold, dtype='float32')
+    accuracy = K.mean(correct_predictions)
+    accuracy *= 100
+    return accuracy
+
+df = clean()
+df['stock_id'] = df['stock_id'].astype('category')
+
+# List to store individual models
+models = []
+
+iters = 0
+max_iters = 2
+
+for stock_id in df['stock_id'].cat.categories:
+    if iters >=  max_iters:
+        break
+
+    print("\nstock id", iters)
+
+    stock_data = df[df['stock_id'] == stock_id].drop(columns=['target', 'stock_id'])
+
+    # Split data into X and y
+    X = stock_data.values
+    y = df[df['stock_id'] == stock_id]['target'].values
+
+    # Reshape X to 3D for LSTM
+    X = np.reshape(X, (X.shape[0], 1, X.shape[1]))
+
+    # Build model
+    model = keras.Sequential()
+    model.add(layers.LSTM(64, input_shape=(1, X.shape[2]), return_sequences=True))  # Experiment with return_sequences
+    model.add(layers.BatchNormalization())
+    model.add(layers.LSTM(32))  # Adding another LSTM layer
+    model.add(layers.BatchNormalization())
+    model.add(layers.Dense(1))  # Output layer for regression
+
+
+    model.compile(
+        optimizer=Adam(learning_rate=0.001),
+        loss='mean_squared_error',
+        metrics=["mae", custom_metric],
+    )
+
+    # Fit the model
+    model.fit(X, y, epochs=10, batch_size=8, validation_split=0.1)
+    
+    # Append the trained model to the list
+    models.append(model)
+
+    iters += 1
+
+# Now you have a list of models, each trained on a specific stock_id
+# You can use these models for prediction on new data
+
+
+'''from cleaning import clean
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras import backend as K
+import numpy as np
 
 def custom_metric(y_true, y_pred):
     threshold = 0.5
@@ -53,3 +120,4 @@ model.compile(
 )
 
 model.fit(x, y, epochs=5, batch_size=16, validation_split=0.1)
+'''
